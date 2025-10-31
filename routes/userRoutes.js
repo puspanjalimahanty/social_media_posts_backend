64f91b12535248a474e5d82aaadac2af13eaad1e
puspanjali mahanty <puspanjalimahanty1@gmail.com>
2025-10-31 20:29:59 +0530

    Fixed backend GET / route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import crypto from "crypto";
-import sendEmail from "../utils/sendEmail.js";
+
 
 
 
@@ -36,45 +36,3 @@ router.post("/login", async (req, res) => {
 });
 
 // Forgot password (reset)
-router.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
-
-  const user = await User.findOne({ email });
-  if (!user) return res.status(404).json({ message: "User not found" });
-
-  // Generate reset token
-  const resetToken = crypto.randomBytes(20).toString("hex");
-  user.resetPasswordToken = resetToken;
-  user.resetPasswordExpire = Date.now() + 3600000; // 1 hour
-  await user.save();
-
-  const resetUrl = `http://localhost:3000/reset-password/${resetToken}`;
-
-  try {
-    await sendEmail(user.email, "Password Reset", `Reset your password: ${resetUrl}`);
-    res.status(200).json({ message: "Reset link sent to email" });
-  } catch (err) {
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-    await user.save();
-    res.status(500).json({ message: "Email could not be sent" });
-  }
-});
-
-router.put("/reset-password/:token", async (req, res) => {
-  const user = await User.findOne({
-    resetPasswordToken: req.params.token,
-    resetPasswordExpire: { $gt: Date.now() },
-  });
-
-  if (!user) return res.status(400).json({ message: "Invalid or expired token" });
-
-  user.password = req.body.password;
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
-  await user.save();
-
-  res.status(200).json({ message: "Password reset successful" });
-});
-
-export default router;
