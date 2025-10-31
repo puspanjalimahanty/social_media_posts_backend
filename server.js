bc38d7f0291d474f23e509a623a377fc72ef9641
puspanjali mahanty <puspanjalimahanty1@gmail.com>
2025-10-31 23:31:25 +0530

    changes in backend

--- server.js
+++ server.js
@@ -5,20 +5,23 @@ import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
-
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+  "https://social-media-posts-frontend-puspanjalimahantys-projects.vercel.app",
+  "http://localhost:5173" // (optional for local testing)
+];
 
 app.use(
   cors({
-    origin: ["https://social-media-posts-frontend.vercel.app"],
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -34,6 +37,5 @@ app.use("/api/posts", postRoutes);
 app.get("/", (req, res) => {
   res.send("✅ Backend is live and running!");
 });
-
 
 export default app;
